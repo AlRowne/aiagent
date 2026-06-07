bbc66049adbd03bcf413240a4ef66abdccadc485
AlRowne <therealgiant@gmail.com>
2026-06-07 13:36:04 +0200

    Add iterative generation loop to handle function calls.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from functools import partial
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -28,41 +28,58 @@ def main():
     ]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
 
-    if not response.usage_metadata:
-        raise RuntimeError("no metadata")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if response.function_calls:
-        function_results = []
-        for function_call in response.function_calls:
-            # print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call, args.verbose)
-            if not function_call_result.parts:
-                raise Exception(f"Function '{function_call.name}' returned no parts")
-            if not function_call_result.parts[0].function_response:
-                raise Exception(
-                    f"Function '{function_call.name}' returned no function_response"
-                )
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception(
-                    f"Function '{function_call.name}' returned empty response"
-                )
-            function_results.append(function_call_result.parts[0])
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate.content:
+                    messages.append(candidate.content)
+
+        if not response.usage_metadata:
+            raise RuntimeError("no metadata")
         if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.function_calls:
+            function_results = []
+            for function_call in response.function_calls:
+                # print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call, args.verbose)
+                if not function_call_result.parts:
+                    raise Exception(
+                        f"Function '{function_call.name}' returned no parts"
+                    )
+                if not function_call_result.parts[0].function_response:
+                    raise Exception(
+                        f"Function '{function_call.name}' returned no function_response"
+                    )
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception(
+                        f"Function '{function_call.name}' returned empty response"
+                    )
+                function_results.append(function_call_result.parts[0])
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
 
+            messages.append(types.Content(role="user", parts=function_results))
+
+        elif response.text:
+            print(response.text)
+            break
+        else:
+            print("No final response after 20 iterations")
+            sys.exit(1)
     else:
-        print(response.text)
+        print("No final response after 20 iterations")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
